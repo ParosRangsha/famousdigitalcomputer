6f500f3742cfd137c783926e318209edb72857e7
Paros Rangsha <paroscrangsha@gmail.com>
2025-03-29 10:17:34 +0000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer fixed bottom-0 left-0 w-full bg-[#999]'>
+      <h1 className='text-center'>Copyright © 2025 Famous Digital Computer and Training Academy. All right reserved</h1>
+    </div>
   )
 }
 
